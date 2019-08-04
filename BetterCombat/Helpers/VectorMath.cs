27cac9d194aae031ce1183f7de88aaa3fcc4aa0a
Bert Guns <bert.guns@ugent.be>
2019-08-04 13:30:31 +0200

    WIP: new soft cover calculations

--- BetterCombat/Helpers/VectorMath.cs
+++ BetterCombat/Helpers/VectorMath.cs
@@ -37,5 +37,63 @@ namespace BetterCombat.Helpers
             d = Mathf.Clamp(d, 0f, length);
             return lineStart + line * d;
         }
+
+        public static void TangentPointsOnCircleFromPoint(Vector2 point, Vector2 circleCenter, float radius, out Vector2 tangent1, out Vector2 tangent2)
+        {
+            Vector2 midPoint = (point + circleCenter) / 2.0f;
+            float midPointCircleRadius = Vector2.Distance(point, circleCenter) / 2.0f;
+
+            CirclesIntersect(circleCenter, radius, midPoint, midPointCircleRadius, out tangent1, out tangent2);
+
+            Vector3.RotateTowards
+            
+        }
+
+        // See http://paulbourke.net/geometry/circlesphere/ , "Intersection of two circles", for explanations of variables
+        public static int CirclesIntersect(Vector2 p0, float r0, Vector2 p1, float r1, out Vector2 t1, out Vector2 t2)
+        {
+            float d = Vector2.Distance(p1, p0);
+            if (d > r0 + r1 || d < Math.Abs(r0 - r1) || (d == 0.0f && r0 == r1))
+            {
+                t1 = Vector2.zero;
+                t2 = Vector2.zero;
+                return 0;
+            }
+
+            float a = ((r0 * r0) - (r1 * r1) + (d * d)) / (2.0f * d);
+            float h = (float)Math.Sqrt((r0 * r0) - (a * a));
+
+            Vector2 p2 = p0 + a * (p1 - p0) / d;
+
+            float t1x = p2.x + h * (p1.y - p0.y) / d;
+            float t1y = p2.y - h * (p1.x - p0.x) / d;
+
+            float t2x = p2.x - h * (p1.y - p0.y) / d;
+            float t2y = p2.y + h * (p1.x - p0.x) / d;
+
+            t1 = new Vector2(t1x, t1y);
+            t2 = new Vector2(t2x, t2y);
+
+            return 2;
+        }
+
+        public static Vector2[] FindEquidistantPointsOnArc(Vector2 startPoint, Vector2 endPoint, Vector2 circleCenter, float radius, int numberOfPoints)
+        {
+            Vector2[] result = new Vector2[numberOfPoints];
+            result[0] = startPoint;
+            result[numberOfPoints - 1] = endPoint;
+
+            float angleBetweenPoints = AngleBetweenPoints(circleCenter, startPoint, endPoint) / numberOfPoints;
+
+            Vector2 currentDirection = (startPoint - circleCenter).normalized;
+            Vector2 endDirection = (endPoint - circleCenter).normalized;
+
+            for (int i = 1; i < numberOfPoints - 1; i++)
+            {
+                currentDirection = Vector3.RotateTowards(currentDirection.To3D(), endDirection.To3D(), angleBetweenPoints, 0.0f).To2D();
+            }
+
+            return result;
+        }
     }
 }
